da5f9e66214e92f5ee72c0f8a15529f4aae0fc07
Vincent <23vincent86@gmail.com>
2020-08-31 16:23:57 +0200

    css

--- js/script.js
+++ js/script.js
@@ -23,5 +23,31 @@
 
 $(document).ready(function() {
     var dataCorrente = moment('2018-01-01');
-    $('h1.month').html(dataCorrente.format('MMMM' + ' ' + 'YYYY'));
+
+    var month = dataCorrente.format('MMMM');
+    var year = dataCorrente.format('YYYY');
+
+    $('h1.month').html(month + ' ' + year);
+
+    var daysMonth = dataCorrente.daysInMonth();
+
+    for (var i = 1; i <= daysMonth; i++) {
+        var source = $("#day-template").html();
+        var template = Handlebars.compile(source);
+
+        var context = {
+            day: addZero(i),
+            month: month
+        };
+        var html = template(context);
+
+        $('.month-list').append(html);
+    }
 });
+
+function addZero(n) {
+    if (n<10) {
+        return '0' + n;
+    }
+    return n;
+};
