fa93addf13add5c34ed9bc26f17505aac507d838
Vincent <23vincent86@gmail.com>
2020-09-01 11:06:17 +0200

    end

--- js/script.js
+++ js/script.js
@@ -21,28 +21,58 @@
 //        }
 //    ]
 
+
 $(document).ready(function() {
-    var dataCorrente = moment('2018-01-01');
-
-    insertDays(dataCorrente);
-    insertHolidays(dataCorrente);
-
-
+    var dataCorrente = moment($("h1.month").attr("data-this-date"));
+    days(dataCorrente);
+    holidays(dataCorrente);
+    $("button#next").click(function() {
+        next(dataCorrente)
+    });
+    $("button#prev").click(function() {
+        prev(dataCorrente)
+    });
 });
-
-// **  FUNZIONI  **
-
-function insertHolidays(data) {
+// ** FUNZIONI **
+function days(data) {
+    $("ul.month-list").empty();
+    var daysMonth = data.daysInMonth();
+    var month = data.format("MMMM");
+    var year = data.format("YYYY");
+    $("h1.month").html(month + " " + year);
+    for (var i = 1; i <= daysMonth; i++) {
+        var source = $("#day-template").html();
+        var template = Handlebars.compile(source);
+        var context = {
+            day: addZero(i),
+            month: month,
+            completeDate: year + "-" + data.format("MM") + "-" + addZero(i),
+        };
+        var html = template(context);
+        $(".month-list").append(html);
+    };
+};
+function addZero(n) {
+    if (n < 10) {
+        return "0" + n;
+    }
+    return n;
+};
+function holidays(data){
     $.ajax(
     {
         url: 'https://flynn.boolean.careers/exercises/api/holidays',
         method: 'GET',
-        data: {
-            year:,
-            month:
+        data:{
+            year: data.year(),
+            month: data.month()
         },
-        success: function(){
-
+        success : function(risposta){
+            for (var i = 0; i < risposta.response.length; i++) {
+              var listItem = $('li[data-completa="'+ risposta.response[i].date + '"]');
+              listItem.addClass('holiday');
+              listItem.append('-' + risposta.response[i].name);
+            }
         },
         error: function (){
             alert('errore')
@@ -50,35 +80,21 @@ function insertHolidays(data) {
     }
     );
 };
-
-function insertDays(data) {
-    var month = data.format('MMMM');
-    var year = data.format('YYYY');
-
-    $('h1.month').html(month + ' ' + year);
-
-    var daysMonth = data.daysInMonth();
-
-    for (var i = 1; i <= daysMonth; i++) {
-
-        var source = $("#day-template").html();
-        var template = Handlebars.compile(source);
-
-        var context = {
-            day: addZero(i),
-            month: month,
-            completeDate : year + '-' + month + '-' + addZero(i)
-        };
-
-        var html = template(context);
-
-        $('.month-list').append(html);
+function next(data) {
+    if (data.month() == 11) {
+        alert("Non puoi proseguire");
+    } else {
+        data.add(1, "months");
+        days(data);
+        holidays(data);
     }
 };
-
-function addZero(n) {
-    if (n<10) {
-        return '0' + n;
+function prev(data) {
+    if (data.month() == 0) {
+        alert("Non puoi proseguire");
+    } else {
+        data.subtract(1, "months");
+        days(data);
+        holidays(data);
     }
-    return n;
 };
